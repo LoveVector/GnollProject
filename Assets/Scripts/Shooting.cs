34be59971c0cc568aefb42e0e6667b47ad45fbd7
LoveVector <superprunejuice@gmail.com>
2021-03-29 17:39:39 +0400

    Progress to Weapon System script, need to figure out how to swap weapons

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,8 +6,23 @@ public class Shooting : MonoBehaviour
 {
     public Animator gunAnim;
     public GameObject bulletImpact;
-    public float fireRate;
+
+    public int damage;
     public int currentAmmo = 50;
+    public int maxAmmo;
+    public int bulletsShot;
+    public int bulletsPerTap;
+
+    public float fireRate;
+    public float spread;
+    public float range;
+
+    public bool allowButtonHold;
+
+    public LayerMask isEnemy;
+
+    bool readyToShoot;
+    bool shooting;
 
     PlayerController player;
 
@@ -20,6 +35,23 @@ public class Shooting : MonoBehaviour
 
     void Update()
     {
+        MyInput();
+        if (currentAmmo > maxAmmo)
+        {
+            currentAmmo = maxAmmo;
+        }
+    }
+
+    void MyInput()
+    {
+        if (allowButtonHold)
+        {
+            shooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else
+        {
+            shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
         Shoot();
     }
 
@@ -27,21 +59,29 @@ public class Shooting : MonoBehaviour
     {
         if (Time.time > fireRateTime)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (shooting && currentAmmo > 0)
             {
-                if (currentAmmo > 0)
+                for (int i = 0; i < bulletsShot; i++)
                 {
-                    Ray ray = player.playerCamera.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+                    float x = Random.Range(-spread, spread);
+                    float y = Random.Range(-spread, spread);
+
+                    Vector3 direction = player.playerCamera.transform.forward + new Vector3(x, y, 0);
+
+                    //Ray ray = player.playerCamera.ViewportPointToRay(new Vector3(.5f * x, .5f * y, 0));
                     RaycastHit hit;
 
-                    if (Physics.Raycast(ray, out hit))
+                    if (Physics.Raycast(player.playerCamera.transform.position, direction, out hit, range))
                     {
-                        //bug.Log("I'm looking at " + hit.transform.name);
+
                         Instantiate(bulletImpact, hit.point, Quaternion.identity);
+                        Debug.Log("I Shot something");
+                        //bug.Log("I'm looking at " + hit.transform.name);
+                        //Look for enemy script and damage him
                     }
                     else
                     {
-                        Debug.Log("I'm looking at nothing");
+                        Debug.Log("I Shot Nothing");
                     }
                     currentAmmo--;
                     gunAnim.SetTrigger("Shoot");
