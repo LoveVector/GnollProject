a0e5482ab5e31a471372cb250374365dae2a6664
LoveVector <superprunejuice@gmail.com>
2021-03-30 23:03:46 +0400

    Added enemy AI with Navmesh, I still have to add health to player and enemies

---
+++ Assets/Scripts/EnemyMeleeAI.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMeleeAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask isPlayer;
+    public LayerMask isGround;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    public float sightRange;
+    public float attackRange;
+
+    public bool playerInSightRange;
+    public bool playerInAttackRange;
+
+    public float health;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, isPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
+
+        if (!playerInAttackRange && !playerInSightRange)
+        {
+            Patrolling();
+        }
+
+        if (!playerInAttackRange && playerInSightRange)
+        {
+            ChasePlayer();
+        }
+
+        if(playerInAttackRange && playerInSightRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, isGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    void ChasePlayer()
+    {
+        Debug.Log("On the way to Player");
+        agent.SetDestination(player.position);
+    }
+
+    void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        if (!alreadyAttacked)
+        {
+            // Attacking
+            Debug.Log("Attacked");
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), .5f);
+        }
+    }
+
+    void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+}
